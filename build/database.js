dedfd202b99f7ca8945fe8310a7c53f1e14953f6
anderson-nunes <andersonribeironunes@hotmail.com>
2023-08-31 20:06:50 -0300

    Aula 01

---
+++ build/database.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.products = exports.users = void 0;
+exports.users = [
+    {
+        id: "and",
+        name: "Anderson",
+        email: "teste@example.com",
+        password: 12345,
+        createdAt: new Date().toISOString(),
+    },
+    {
+        id: "eve",
+        name: "Everton",
+        email: "teste@example.com",
+        password: 54321,
+        createdAt: new Date().toISOString(),
+    },
+];
+exports.products = [
+    {
+        id: "eve",
+        name: "Everton",
+        price: 50,
+        description: "chablau",
+        imageUrl: "http://",
+    },
+    {
+        id: "pat",
+        name: "Patricia",
+        price: 70,
+        description: "chablau",
+        imageUrl: "http://",
+    },
+];
