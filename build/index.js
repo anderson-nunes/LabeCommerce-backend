79b422c0c9431c074abd244c4f49d681c343e6bb
anderson-nunes <andersonribeironunes@hotmail.com>
2023-09-11 22:14:32 -0300

    Exercicio 03 cadastro de usuario e produto

--- build/index.js
+++ build/index.js
@@ -1,6 +1,62 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = require("./database");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.listen(3003, () => {
+    console.log("Servidor rodando na porta 3003");
+});
+app.get("/teste", (req, res) => {
+    // console.log(req.body);
+    res.send("testado!");
+});
+/////////////////////////////////////////////////////////
+app.get("/users", (req, res) => {
+    const result = database_1.users;
+    res.status(200).send(result);
+});
+app.post("/users", (req, res) => {
+    const { id, name, email, password, createdAt } = req.body;
+    const newUsers = {
+        id,
+        name,
+        email,
+        password,
+        createdAt,
+    };
+    database_1.users.push(newUsers);
+    res.status(2001).send("Usuário registrado com sucesso");
+});
+////////////////////////////////////////////////////////////////////////////////////////
+app.get("/products", (req, res) => {
+    const result = database_1.products;
+    res.status(200).send(result);
+});
+app.get("/products/search", (req, res) => {
+    const query = req.query.q;
+    const productsByProducts = database_1.products.filter((product) => product.name.toLowerCase() === query.toLowerCase());
+    res.status(200).send(productsByProducts);
+});
+app.post("/products", (req, res) => {
+    const { id, name, price, description, imageUrl } = req.body;
+    const newProducts = {
+        id,
+        name,
+        price,
+        description,
+        imageUrl,
+    };
+    database_1.products.push(newProducts);
+    res.status(2001).send("Produto registrado com sucesso");
+});
 // console.log(users, products, getAllUsers, getAllProduct);
 // console.log(createUser("u003", "Astrodev", "astrodev@email.com", 123399));
 // console.log(
@@ -12,5 +68,5 @@ const database_1 = require("./database");
 //     "https://images.unsplash.com/photo"
 //   )
 // );
-console.log((0, database_1.searchProductsByName)("cadeira"));
+// console.log(searchProductsByName("cadeira"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,87 @@ import {
   createProduct,
   getAllProduct,
   searchProductsByName,
+  TUser,
 } from "./database";
+
+import express, { Request, Response } from "express";
+
+import cors from "cors";
+import { TProducts } from "./types";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {
+  console.log("Servidor rodando na porta 3003");
+});
+
+app.get("/teste", (req: Request, res: Response) => {
+  // console.log(req.body);
+  res.send("testado!");
+});
+
+/////////////////////////////////////////////////////////
+
+app.get("/users", (req: Request, res: Response) => {
+  const result: TUser[] = users;
+
+  res.status(200).send(result);
+});
+
+app.post("/users", (req: Request, res: Response) => {
+  const { id, name, email, password, createdAt }: TUser = req.body;
+
+  const newUsers: TUser = {
+    id,
+    name,
+    email,
+    password,
+    createdAt,
+  };
+
+  users.push(newUsers);
+  res.status(2001).send("Usuário registrado com sucesso");
+});
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+app.get("/products", (req: Request, res: Response) => {
+  const result: TProducts[] = products;
+
+  res.status(200).send(result);
+});
+
+app.get("/products/search", (req: Request, res: Response) => {
+  const query: string = req.query.q as string;
+
+  const productsByProducts: TProducts[] = products.filter(
+    (product) => product.name.toLowerCase() === query.toLowerCase()
+  );
+
+  res.status(200).send(productsByProducts);
+});
+
+app.post("/products", (req: Request, res: Response) => {
+  const { id, name, price, description, imageUrl }: TProducts = req.body;
+
+  const newProducts: TProducts = {
+    id,
+    name,
+    price,
+    description,
+    imageUrl,
+  };
+
+  products.push(newProducts);
+  res.status(2001).send("Produto registrado com sucesso");
+});
 
 // console.log(users, products, getAllUsers, getAllProduct);
 
@@ -22,4 +102,4 @@ import {
 //   )
 // );
 
-console.log(searchProductsByName("cadeira"));
+// console.log(searchProductsByName("cadeira"));
